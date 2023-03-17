41daa76dfddb03db591ea501724c1c6e2df03362
Fabien Taillon <ftaillon@gmail.com>
2023-03-17 16:58:55 +0100

    Init

---
+++ index.js
@@ -0,0 +1,8 @@
+import { helloThere } from './modules/vscode-ui-toolkit.js';
+
+//console.log('index');
+//helloThere();
+
+export function helloModule() {
+    helloThere();
+}
---
+++ modules/vscode-ui-toolkit.js
@@ -0,0 +1,6 @@
+//import { provideVSCodeDesignSystem, vsCodeButton } from "@vscode/webview-ui-toolkit";
+//provideVSCodeDesignSystem().register(vsCodeButton());
+
+export function helloThere() {
+    console.log('Hello World');
+}
